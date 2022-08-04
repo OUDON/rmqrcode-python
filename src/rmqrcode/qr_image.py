fbb9015aa597f060ea3c4ee1722dcc082bed421d
Takahiro Tomita <ta_tomita@yumemi.co.jp>
2022-08-04 10:42:41 +0900

    refactor: Use with-quiet-zone in QRIMage#_make_image

--- src/rmqrcode/qr_image.py
+++ src/rmqrcode/qr_image.py
@@ -6,10 +6,11 @@ from .enums.color import Color
 class QRImage:
     def __init__(self, qr, module_size=10):
         self._module_size = module_size
+        qr_list = qr.to_list()
         self._img = Image.new(
-            "RGB", ((qr.width() + 4) * module_size, (qr.height() + 4) * module_size), (255, 255, 255)
+            "RGB", (len(qr_list[0]) * module_size, len(qr_list) * module_size), (255, 255, 255)
         )
-        self._make_image(qr)
+        self._make_image(qr_list)
 
     def show(self):
         self._img.show()
@@ -26,25 +27,17 @@ class QRImage:
     def save(self, name):
         self._img.save(name)
 
-    def _make_image(self, qr):
+    def _make_image(self, qr_list):
         draw = ImageDraw.Draw(self._img)
-        for y in range(qr.height()):
-            for x in range(qr.width()):
-                r, g, b = 125, 125, 125
-                if qr.value_at(x, y) == Color.BLACK:
-                    r, g, b = 0, 0, 0
-                elif qr.value_at(x, y) == Color.WHITE:
-                    r, g, b, = (
-                        255,
-                        255,
-                        255,
-                    )
+        for y in range(len(qr_list)):
+            for x in range(len(qr_list[0])):
+                r, g, b = (0, 0, 0) if qr_list[y][x] else (255, 255, 255)
                 draw.rectangle(
                     xy=(
-                        (x + 2) * self._module_size,
-                        (y + 2) * self._module_size,
-                        (x + 1 + 2) * self._module_size,
-                        (y + 1 + 2) * self._module_size,
+                        x * self._module_size,
+                        y * self._module_size,
+                        (x + 1) * self._module_size,
+                        (y + 1) * self._module_size,
                     ),
                     fill=(r, g, b),
                 )
