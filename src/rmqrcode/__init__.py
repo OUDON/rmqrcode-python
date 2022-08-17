dbc9311622bc8c30625d5c39e44975dab36e0a1c
Takahiro Tomita <ta_tomita@yumemi.co.jp>
2022-08-17 11:50:26 +0900

    fix: Add encoder into rmqrcode/__init__.py

--- src/rmqrcode/__init__.py
+++ src/rmqrcode/__init__.py
@@ -1,5 +1,6 @@
 from .format.error_correction_level import ErrorCorrectionLevel
 from .qr_image import QRImage
 from .rmqrcode import DataTooLongError, FitStrategy, IllegalVersionError, rMQR
+from . import encoder
 
-__all__ = ("rMQR", "DataTooLongError", "FitStrategy", "IllegalVersionError", "QRImage", "ErrorCorrectionLevel")
+__all__ = ("rMQR", "DataTooLongError", "FitStrategy", "IllegalVersionError", "QRImage", "ErrorCorrectionLevel", "encoder")
