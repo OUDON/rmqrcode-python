4d14596313e7e2a072b33136c468eab83885136c
Takahiro Tomita <ta_tomita@yumemi.co.jp>
2022-09-04 11:43:09 +0900

    feat: Add more testcases into segments_test.py

--- tests/segments_test.py
+++ tests/segments_test.py
@@ -4,12 +4,48 @@ import pytest
 
 
 class TestSegments:
-    def test_can_optimize_segments(self):
+    def test_can_optimize_segments_numeric_and_byte(self):
         optimizer = SegmentOptimizer()
         segments = optimizer.compute("123Abc", "R7x43")
         assert segments == [
             {"data": "123", "encoder_class": encoder.NumericEncoder},
             {"data": "Abc", "encoder_class": encoder.ByteEncoder},
+        ]
+
+    def test_can_optimize_segments_alphanumeric_and_kanji(self):
+        optimizer = SegmentOptimizer()
+        segments = optimizer.compute("17:30集合", "R7x59")
+        assert segments == [
+            {"data": "17:30", "encoder_class": encoder.AlphanumericEncoder},
+            {"data": "集合", "encoder_class": encoder.KanjiEncoder},
+        ]
+
+    def test_can_optimize_segments_numeric_only(self):
+        optimizer = SegmentOptimizer()
+        segments = optimizer.compute("123456", "R7x59")
+        assert segments == [
+            {"data": "123456", "encoder_class": encoder.NumericEncoder},
+        ]
+
+    def test_can_optimize_segments_alphanumeric_only(self):
+        optimizer = SegmentOptimizer()
+        segments = optimizer.compute("HTTPS://", "R7x59")
+        assert segments == [
+            {"data": "HTTPS://", "encoder_class": encoder.AlphanumericEncoder},
+        ]
+
+    def test_can_optimize_segments_byte_only(self):
+        optimizer = SegmentOptimizer()
+        segments = optimizer.compute("1+zY!a:K", "R7x59")
+        assert segments == [
+            {"data": "1+zY!a:K", "encoder_class": encoder.ByteEncoder},
+        ]
+
+    def test_can_optimize_segments_kanji_only(self):
+        optimizer = SegmentOptimizer()
+        segments = optimizer.compute("漢字", "R7x59")
+        assert segments == [
+            {"data": "漢字", "encoder_class": encoder.KanjiEncoder},
         ]
 
     def test_compute_length(self):
